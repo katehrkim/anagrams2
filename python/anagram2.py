4ce732204795481281e037ee82aad8a8ff4d9095
Kate Kim <katehrkim@gmail.com>
2021-05-25 17:42:06 -0400

    Kate's answer

--- python/anagram2.py
+++ python/anagram2.py
@@ -1,4 +1,16 @@
 # Don't forget to run the tests (and create some of your own)
 
 def anagrams_for(word, list_of_words):
-		# your code here
+	output = []
+	for element in list_of_words:
+		raw1 = element.lower().replace(" ", "")
+		raw2 = word.lower().replace(" ", "")
+		list1 = []
+		list2 = []
+		for letter in raw1:
+			list1.append(letter)
+		for letter in raw2:
+			list2.append(letter)
+		if sorted(list1) == sorted(list2):
+			output.append(element)
+	return output
--- python/anagram2_spec.py
+++ python/anagram2_spec.py
@@ -1,8 +1,15 @@
 # Can you translate this driver code to unit tests?
-
+import unittest
 from anagram2 import anagrams_for 
 
 list_of_words = ["threads", "trashed", "hardest", "hatreds", "hounds"]
 
-print(anagrams_for("threads", list_of_words) == ["threads", "trashed", "hardest", "hatreds"])
-print(anagrams_for("apple", list_of_words) == [])
+class Anagram2TestCase(unittest.TestCase):
+    """Tests for anagram2.py"""
+    def test_one(self):
+        self.assertEqual(anagrams_for("threads", list_of_words), ["threads", "trashed", "hardest", "hatreds"])
+    def test_two(self):
+        self.assertEqual(anagrams_for("apple", list_of_words), [])
+
+if __name__ == '__main__':
+    unittest.main()
